a7afd77addcf15bc69f047767ea971a93230d1dc
Zachary Kirtz <zach.kirtz@gmail.com>
2023-08-27 20:35:55 -0500

    mem: Add new results relating to regions and devices.

--- Libraries/RiscvLib/Include/RiscvEmu/mem/mem_Result.h
+++ Libraries/RiscvLib/Include/RiscvEmu/mem/mem_Result.h
@@ -12,5 +12,17 @@ constexpr inline Word ModuleId = 2;
 
 class ResultBadMisalignedAddress : public result::ErrorBase<detail::ModuleId, 1> {};
 
+class ResultBadAddress : public result::ErrorBase<detail::ModuleId, 2> {};
+
+class ResultRegionAlreadyExists : public result::ErrorBase<detail::ModuleId, 3> {};
+
+class ResultRegionDoesNotExist : public result::ErrorBase<detail::ModuleId, 4> {};
+
+class ResultInvalidRegionType : public result::ErrorBase<detail::ModuleId, 5> {};
+
+class ResultDeviceAlreadyExists : public result::ErrorBase<detail::ModuleId, 6> {};
+
+class ResultDeviceDoesNotExist : public result::ErrorBase<detail::ModuleId, 7> {};
+
 } // namespace mem
 } // namespace riscv
