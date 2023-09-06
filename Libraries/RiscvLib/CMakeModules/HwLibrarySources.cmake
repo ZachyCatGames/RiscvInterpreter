e1746ba28606b599924490c381b613a240a486a7
Zachary Kirtz <zach.kirtz@gmail.com>
2023-09-06 18:35:39 -0500

    build: Add hw library.

---
+++ Libraries/RiscvLib/CMakeModules/HwLibrarySources.cmake
@@ -0,0 +1,9 @@
+set(RISCV_HW_LIBRARY_HEADERS
+    "${PROJECT_SOURCE_DIR}/Include/RiscvEmu/hw/hw_IDevice.h"
+    "${PROJECT_SOURCE_DIR}/Include/RiscvEmu/hw/hw_Scheduler.h"
+)
+
+set(RISCV_HW_LIBRARY_SOURCES
+    "${PROJECT_SOURCE_DIR}/Sources/hw/hw_Scheduler.cpp"
+)
+
