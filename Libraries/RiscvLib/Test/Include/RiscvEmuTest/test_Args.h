0cf75f133d4652f623bd86f06da37f7692e74fbc
Zachary Kirtz <zach.kirtz@gmail.com>
2023-09-13 19:05:58 -0500

    test: Add support for iterating Args.

--- Libraries/RiscvLib/Test/Include/RiscvEmuTest/test_Args.h
+++ Libraries/RiscvLib/Test/Include/RiscvEmuTest/test_Args.h
@@ -6,6 +6,63 @@ namespace test {
 
 class Args {
 public:
+    class ForwardIterator {
+    public:
+        constexpr ForwardIterator& operator++() noexcept {
+            ++m_Argv;
+            return *this;
+        }
+
+        constexpr ForwardIterator operator++(int) noexcept {
+            auto old = *this;
+            ++m_Argv;
+            return old;
+        }
+
+        constexpr ForwardIterator& operator--() noexcept {
+            --m_Argv;
+            return *this;
+        }
+
+        constexpr ForwardIterator operator--(int) noexcept {
+            auto old = *this;
+            --m_Argv;
+            return old;
+        }
+
+        constexpr bool operator==(const ForwardIterator& rhs) const noexcept {
+            return this->m_Argv == rhs.m_Argv;
+        }
+
+        constexpr std::string_view operator*() const noexcept { return *m_Argv; }
+        constexpr const char** operator->() const noexcept { return m_Argv; }
+
+        friend constexpr ForwardIterator operator+(const ForwardIterator& lhs, int rhs) noexcept {
+            return ForwardIterator(lhs.m_Argv + rhs);
+        }
+
+        friend constexpr ForwardIterator operator+(int lhs, const ForwardIterator& rhs) noexcept {
+            return ForwardIterator(rhs.m_Argv + lhs);
+        }
+
+        friend constexpr ForwardIterator operator-(const ForwardIterator& lhs, int rhs) noexcept {
+            return ForwardIterator(lhs.m_Argv - rhs);
+        }
+
+        constexpr auto operator<=>(const ForwardIterator& rhs) noexcept {
+            return this->m_Argv <=> rhs.m_Argv;
+        }
+    private:
+        friend class Args;
+        constexpr ForwardIterator(const char** argv) noexcept :
+            m_Argv(argv) {}
+    private:
+        const char** m_Argv;
+    };
+public:
+    using iterator = ForwardIterator;
+    using const_iterator = ForwardIterator;
+public:
     constexpr Args(int argc, const char** argv) noexcept :
         m_Argc(argc),
         m_Argv(argv) {}
@@ -14,6 +71,13 @@ public:
     constexpr std::string_view operator[](std::size_t offs) const noexcept {
         return std::string_view(m_Argv[offs]);
     }
+
+    constexpr iterator begin() const noexcept { return ForwardIterator(m_Argv); }
+    constexpr iterator cbegin() const noexcept { return this->begin(); }
+
+    constexpr iterator end() const noexcept { return ForwardIterator(m_Argv + m_Argc); }
+    constexpr iterator cend() const noexcept { return this->end(); }
+
 private:
     int m_Argc;
     const char ** m_Argv;
