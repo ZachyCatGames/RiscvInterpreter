21b0beec1eb451f3e41abfa9df8d94e12a4cacb2
Zachary Kirtz <zach.kirtz@gmail.com>
2023-10-16 21:35:42 -0500

    cpu: Hart: Use new Mapped MemoryManager functions for MappedRead/Write functions.

--- Libraries/RiscvLib/Sources/cpu/Hart/cpu_UserApi.cpp
+++ Libraries/RiscvLib/Sources/cpu/Hart/cpu_UserApi.cpp
@@ -31,35 +31,35 @@ Result Hart::ReadCSR([[maybe_unused]] int index, [[maybe_unused]] NativeWord* pO
 }
 
 Result Hart::MappedReadByte(Byte* pOut, Address addr) {
-    return m_MemMgr.ReadByte(pOut, addr, PrivilageLevel::Machine);
+    return m_MemMgr.MappedReadByte(pOut, addr);
 }
 
 Result Hart::MappedReadHWord(HWord* pOut, Address addr) {
-    return m_MemMgr.ReadHWord(pOut, addr, PrivilageLevel::Machine);
+    return m_MemMgr.MappedReadHWord(pOut, addr);
 }
 
 Result Hart::MappedReadWord(Word* pOut, Address addr) {
-    return m_MemMgr.ReadWord(pOut, addr, PrivilageLevel::Machine);
+    return m_MemMgr.MappedReadWord(pOut, addr);
 }
 
 Result Hart::MappedReadDWord(DWord* pOut, Address addr) {
-    return m_MemMgr.ReadDWord(pOut, addr, PrivilageLevel::Machine);
+    return m_MemMgr.MappedReadDWord(pOut, addr);
 }
 
 Result Hart::MappedWriteByte(Byte in, Address addr) {
-    return m_MemMgr.WriteByte(in, addr, PrivilageLevel::Machine);
+    return m_MemMgr.MappedWriteByte(in, addr);
 }
 
 Result Hart::MappedWriteHWord(HWord in, Address addr) {
-    return m_MemMgr.WriteHWord(in, addr, PrivilageLevel::Machine);
+    return m_MemMgr.MappedWriteHWord(in, addr);
 }
 
 Result Hart::MappedWriteWord(Word in, Address addr) {
-    return m_MemMgr.WriteWord(in, addr, PrivilageLevel::Machine);
+    return m_MemMgr.MappedWriteWord(in, addr);
 }
 
 Result Hart::MappedWriteDWord(DWord in, Address addr) {
-    return m_MemMgr.WriteDWord(in, addr, PrivilageLevel::Machine);
+    return m_MemMgr.MappedWriteDWord(in, addr);
 }
 
 } // namespace cpu
