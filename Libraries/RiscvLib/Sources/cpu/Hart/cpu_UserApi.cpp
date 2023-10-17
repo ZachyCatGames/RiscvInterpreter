1449b11e29759e31daa827c9e922d0d240773579
Zachary Kirtz <zach.kirtz@gmail.com>
2023-10-16 21:04:04 -0500

    cpu: Hart: Actually implement memory access User API functions.

--- Libraries/RiscvLib/Sources/cpu/Hart/cpu_UserApi.cpp
+++ Libraries/RiscvLib/Sources/cpu/Hart/cpu_UserApi.cpp
@@ -22,12 +22,44 @@ Result Hart::ExecuteInstAtPc() {
     return this->ExecuteInst(inst);
 }
 
-Result Hart::WriteCSR(int index, NativeWord value) {
+Result Hart::WriteCSR([[maybe_unused]] int index, [[maybe_unused]] NativeWord value) {
     return ResultNotImplemented();
 }
 
-Result Hart::ReadCSR(int index, NativeWord* pOut) {
+Result Hart::ReadCSR([[maybe_unused]] int index, [[maybe_unused]] NativeWord* pOut) {
     return ResultNotImplemented();
+}
+
+Result Hart::MappedReadByte(Byte* pOut, Address addr) {
+    return m_MemMgr.ReadByte(pOut, addr, PrivilageLevel::Machine);
+}
+
+Result Hart::MappedReadHWord(HWord* pOut, Address addr) {
+    return m_MemMgr.ReadHWord(pOut, addr, PrivilageLevel::Machine);
+}
+
+Result Hart::MappedReadWord(Word* pOut, Address addr) {
+    return m_MemMgr.ReadWord(pOut, addr, PrivilageLevel::Machine);
+}
+
+Result Hart::MappedReadDWord(DWord* pOut, Address addr) {
+    return m_MemMgr.ReadDWord(pOut, addr, PrivilageLevel::Machine);
+}
+
+Result Hart::MappedWriteByte(Byte in, Address addr) {
+    return m_MemMgr.WriteByte(in, addr, PrivilageLevel::Machine);
+}
+
+Result Hart::MappedWriteHWord(HWord in, Address addr) {
+    return m_MemMgr.WriteHWord(in, addr, PrivilageLevel::Machine);
+}
+
+Result Hart::MappedWriteWord(Word in, Address addr) {
+    return m_MemMgr.WriteWord(in, addr, PrivilageLevel::Machine);
+}
+
+Result Hart::MappedWriteDWord(DWord in, Address addr) {
+    return m_MemMgr.WriteDWord(in, addr, PrivilageLevel::Machine);
 }
 
 } // namespace cpu
