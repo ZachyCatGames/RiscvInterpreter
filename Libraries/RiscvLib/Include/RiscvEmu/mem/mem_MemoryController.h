11fefc0b293ee779b4fe22296aa7f6f1bf9c6863
Zachary Kirtz <zach.kirtz@gmail.com>
2024-05-04 16:56:05 -0500

    mem: MemoryController: Add docs for Load/Store/LoadReserve/StoreConditional.

--- Libraries/RiscvLib/Include/RiscvEmu/mem/mem_MemoryController.h
+++ Libraries/RiscvLib/Include/RiscvEmu/mem/mem_MemoryController.h
@@ -60,15 +60,87 @@ public:
     /** Add an MMIO device. */
     Result AddMmioDev(IMmioDev* dev, Address addr);
 
+    /**
+     * @brief Perform a load from memory.
+     * 
+     * This loads a WordType from memory.
+     * 
+     * WordType can be any of the following: 
+     * Byte, HWord, Word, DWord, ByteS, HWordS, WordS, DWordS, float, or double.
+     * 
+     * Depending on the device being written to, addr may or may not be required to 
+     * be aligned to some boundry.
+     * 
+     * This operation is not atomic.
+     * 
+     * @param pOut Location to store the loaded word.
+     * @param addr Byte address of word to load.
+     * @return Result code.
+    */
     template<typename WordType>
     Result Load(WordType* pOut, Address addr) { return this->LoadImpl(pOut, addr); }
 
+    /**
+     * @brief Perform a store to memory.
+     * 
+     * This stores a WordType to memory.
+     * 
+     * WordType can be any of the following:
+     * Byte, HWord, Word, DWord, ByteS, HWordS, WordS, DWordS, float, or double.
+     * 
+     * Depending on the device being written to, addr may or may not be required to 
+     * be aligned to some boundry.
+     * 
+     * This operation is atomic.
+     * 
+     * @param in   Word to store to memory.
+     * @param addr Byte address to store word at.
+     * @return Result code.
+    */
     template<typename WordType>
     Result Store(WordType in, Address addr) { return this->StoreImpl(in, addr); }
 
+    /**
+     * @brief Perform a load from memory and register a reservation.
+     * 
+     * This loads a WordType from memory.
+     * 
+     * WordType can be any of the following: 
+     * Byte, HWord, Word, DWord, ByteS, HWordS, WordS, DWordS, float, or double.
+     * 
+     * After performing the load a reservation entry for addr will be created.
+     * 
+     * The provided address must be aligned to sizeof(WordType).
+     * 
+     * This operation is atomic.
+     * 
+     * @param client    Client ID to register a reservation for.
+     * @param pOut      Location to store loaded word.
+     * @param addr      Byte address of word to load and add a registration for.
+     * @return Result code.
+    */
     template<typename WordType>
     Result LoadReserve(int client, WordType* pOut, Address addr);
 
+    /**
+     * @brief Perform a conditional store based on reservation entries.
+     * 
+     * This stores a WordType to memory only if the specified client has a reservation 
+     * for the provided address. If the store succeeds the provided word is overwritten
+     * with 0, otherwise it is overwritten with 1.
+     * 
+     * WordType can be any of the following: 
+     * Byte, HWord, Word, DWord, ByteS, HWordS, WordS, DWordS, float, or double.
+     * 
+     * The provided address must be aligned to sizeof(WordType).
+     * 
+     * This operation is atomic.
+     * 
+     * @param client    Client ID to check reservation for.
+     * @param pInOut    Location containing the word to be written.
+     * @param addr      Byte address to store word at.
+     * @return Result code.
+    */
     template<typename WordType>
     Result StoreConditional(int client, WordType* pInOut, Address addr);
 private:
